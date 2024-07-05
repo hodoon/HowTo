c816f0cc80ed6f6646c3bacd5caba1aa1753916e
hodoon <ehgns79513@gmail.com>
2024-07-05 18:16:49 +0900

    HowToProj

---
+++ src/main/java/com/example/HowToProj/HowToProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.HowToProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HowToProj/HowToProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HowToProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
